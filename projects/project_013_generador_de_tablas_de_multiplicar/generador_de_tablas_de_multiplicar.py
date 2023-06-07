29d34ef7938c66f858baa9ff1b167f8b7777a533
M412KVZ <glow_of_old_suns@pm.me>
2023-06-07 15:16:38 -0600

    mejora v1.1.2

--- projects/project_013_generador_de_tablas_de_multiplicar/generador_de_tablas_de_multiplicar.py
+++ projects/project_013_generador_de_tablas_de_multiplicar/generador_de_tablas_de_multiplicar.py
@@ -6,25 +6,25 @@
 # date: wed/07/jun/2023
 # description: generador de tablas de multiplicar con cualquir numero entero
 # v1.1.0 creación del archivo
-# v1.1.1 agregado manejo de timepos, titulos y elección de numeros de inicio y final de la tabla 
+# v1.1.1 agregado manejo de timepos, títulos y elección de numeros de inicio y final de la tabla 
+# v1.1.2 agregar color al texto
 
 # pendiente
-#   agregar color al texto
 #   bucle para elección de una nueva tabla o salir
 
 # ----- Inicio -----
 
 import time  # v1.1.1 se agrega el modulo time para gestion de tiempos
 
-print("\nBienvenido al Generador de Tablas de Multiplicar\n")  # v1.1.1 se agrego un titulo
+print("\033[;32m" + "\nBienvenido al Generador de Tablas de Multiplicar" + "\033[0;m")  # v1.1.1 se agrego un titulo, v1.1.2
 
-numero = int(input("\nIntroduce un numero entero: "))  # introducir cualquier numero para generar la tabla
+numero = int(input("\033[;33m" + "\nIntroduce un numero entero:" + " \033[0;m"))  # introducir cualquier numero para generar la tabla, v1.1.2
 
 numero_inicio = int(input("\nIntroduce el numero de inicio de la Tabla: "))  # v1.1.1 introducir cualquier numero entero desde el que se quiere iniciar la tabla
 
 numero_final = int(input("\nIntroduce el numero final de la Tabla: "))  # v1.1.1 introducir cualquier numero entero hasta el que se quiere generar la tabla
 
-print("\nGenerando Tabla...\n")  # v1.1.1 titulo
+print("\033[;36m" + "\nGenerando Tabla..." + "\033[0;m\n")  # v1.1.1 titulo, v1.1.2
 
 time.sleep(2)  # v1.1.1 tempo de retraso 2 segundos
 
@@ -33,7 +33,7 @@ for i in range(numero_inicio,numero_final+1):  # codigo para la generacion de la
     resultado = i * numero  # generación del cálculo
     print(("%d x %d = %d") % (numero, i, resultado))  # impresión del resultado 
 
-print("\nTabla Generada... saliendo del programa\n")  # v1.1.1 mensaje de termino y salida del programa
+print("\033[;35m" + "\nTabla Generada... saliendo del programa" + "\033[0;m\n")  # v1.1.1 mensaje de termino y salida del programa, v1.1.2
 
 time.sleep(2)  # v1.1.1 tiempo de espera antes de salir del programa
 
