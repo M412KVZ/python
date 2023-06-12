bdf22bd0ed870dbb3eebab13fe38aa35d0b8b83b
M412KVZ <glow_of_old_suns@pm.me>
2023-06-12 10:31:35 -0600

    agregado bucle recursivo y color al texto

--- projects/project_013_generador_de_tablas_de_multiplicar/generador_de_tablas_de_multiplicar.py
+++ projects/project_013_generador_de_tablas_de_multiplicar/generador_de_tablas_de_multiplicar.py
@@ -8,40 +8,38 @@
 # v1.1.0 creación del archivo
 # v1.1.1 agregado manejo de timepos, títulos y elección de numeros de inicio y final de la tabla 
 # v1.1.2 agregar color al texto
-
-# pendiente
-#   bucle para elección de una nueva tabla o salir
+# v1.1.3 bucle para elección de una nueva tabla o salir
 
 # ----- Inicio -----
 
 import time  # v1.1.1 se agrega el modulo time para gestion de tiempos
 
-# Definiendo la función menu
-def menu():
-    print("\033[;32m" + "\nBienvenido al Generador de Tablas de Multiplicar" + "\033[0;m")  # v1.1.1 se agrego un titulo, v1.1.2
-    print("\n¿Que desea Hacer?")
-    print("\n[1] Generar Tabla")
-    print("[2] Salir")
-    global hacer
-    hacer = input()
-    hacer = int(hacer)
+
+print("\033[;32m" + "\nBienvenido al Generador de Tablas de Multiplicar" + "\033[0;m")  # v1.1.1 se agrego un titulo, v1.1.2
+
+def menu():  # definiendo la función menu
+    print("\033[;34m" + "\n¿Que desea Hacer?" + "\033[0;m") 
+    print("\033[;32m" + "\n[1] " + "\033[0;m" "Generar Tabla")
+    print("\033[;32m" + "[2] " + "\033[0;m" "Salir")
+    global hacer 
+    hacer = int(input("\033[;34m" + "\nPor favor elija una opción: " + " \033[0;m"))
     if (hacer == 1):
-        tabla()
-    elif (hacer == 2):
-        print("Saliendo...")
-        time.sleep(1)
+        tabla()  # llamando a la función tabla
+    elif (hacer == 2):  # salir
+        print("\033[;36m" + "\nSaliendo..." + " \033[0;m")
+        time.sleep(1)  # tiempomde espera antes de salir
         exit
-    else:
-        print("\nLa opcionque elegiste no es correcta")
-        menu()
+    else:  # 
+        print("\033[;31m" + "\nLa opcionque elegiste no es correcta" + "\033[;0m")  # v1.1.2, v1.1.3
+        menu()  # llamando a la función menu
 
 # Definiendo la Función Tabla
 def tabla():
     numero = int(input("\033[;33m" + "\nIntroduce un numero entero:" + " \033[0;m"))  # introducir cualquier numero para generar la tabla, v1.1.2
 
-    numero_inicio = int(input("\nIntroduce el numero de inicio de la Tabla: "))  # v1.1.1 introducir cualquier numero entero desde el que se quiere iniciar la tabla
+    numero_inicio = int(input("\033[;33m" + "\nIntroduce el numero de inicio de la Tabla: " + " \033[0;m"))  # v1.1.1 introducir cualquier numero entero desde el que se quiere iniciar la tabla
 
-    numero_final = int(input("\nIntroduce el numero final de la Tabla: "))  # v1.1.1 introducir cualquier numero entero hasta el que se quiere generar la tabla
+    numero_final = int(input("\033[;33m" + "\nIntroduce el numero final de la Tabla: " + " \033[0;m"))  # v1.1.1 introducir cualquier numero entero hasta el que se quiere generar la tabla
 
     print("\033[;36m" + "\nGenerando Tabla..." + "\033[0;m\n")  # v1.1.1 titulo, v1.1.2
 
@@ -52,25 +50,25 @@ def tabla():
         resultado = i * numero  # generación del cálculo
         print(("%d x %d = %d") % (numero, i, resultado))  # impresión del resultado 
 
-    print("\033[;35m" + "\nTabla Generada..." + "\033[0;m\n")  # v1.1.1 mensaje de termino y salida del programa, v1.1.2
-    pregunta_1()
+    print("\033[;35m" + "\nTabla Generada..." + "\033[0;m")  # v1.1.1 mensaje de termino y salida del programa, v1.1.2
+    time.sleep(1)  # tiempo de espera antes de salir
+    pregunta()  # llamando a la función pregunta v1.1.3
 
-def pregunta_1():
-    print("¿Desea generar otra Tabla?\n")
-    print("[1] Si")
-    print("[2] No, Salir")
-    global pregunta_3
-    pregunta_3 = input()
-    pregunta_3 = int(pregunta_3)
-    if (pregunta_3 == 1):
-        tabla()
-    elif (pregunta_3 == 2):
-        print("Saliendo...")
-        time.sleep(2)
+def pregunta():  # definiendo la función pregunta v1.1.3
+    print("\033[;34m" + "\n¿Desea generar otra Tabla?\n" + "\033[0;m")
+    print("\033[;35m" + "[1] " + "\033[0;m" + "Si")
+    print("\033[;35m" + "[2] " + "\033[0;m" +  "No, Salir")
+    global pregunta1
+    pregunta1 = int(input("\033[;34m" + "\nPor favor elija una opción: " + "\033[0;m"))
+    if (pregunta1 == 1):
+        tabla()  # llmanado a la función tabla
+    elif (pregunta1 == 2):
+        print("\033[;36m" + "\nSaliendo..." + " \033[0;m")
+        time.sleep(1)  # tiempo de espera antes de salir del programa
         exit
     else:
-        print("\nLa opcionque elegiste no es correcta")
-        pregunta_1()
+        print("\033[;31m" + "\nLa opcionque elegiste no es correcta" + "\033[;0m")  # v1.1.2, v1.1.3
+        pregunta()
 
 # Llamando a la funcion menu 
 menu()
