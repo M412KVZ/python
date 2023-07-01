1810bb5a3d00cb4eb0c9c1d3ee90fbaa134d5164
M412KVZ <glow_of_old_suns@pm.me>
2023-06-30 18:39:07 -0600

    inicio

---
+++ projects/project_017_sudoku/sudoku.py
@@ -0,0 +1,26 @@
+#! python3
+# -*- coding: utf-8 -*-
+# author: Gerry studios
+# links: https://www.youtube.com/watch?v=tDxOk9TAnJ0
+# modificacion y mejora: glow 
+# date: sat/17/jun/2023/0635
+# description: resolucion de sodokus de 3x3
+# v1.1.0 creación del archivo y codigo de inicio
+
+# ----- Inicio -----
+
+SUDOKU = [
+    [0,0,0,0,7,1,0,2,0],
+    [0,0,0,0,0,0,8,0,0],
+    [0,9,8,2,0,0,0,0,0],
+    [7,8,0,9,0,0,3,0,0],
+    [0,0,0,0,0,7,0,0,0],
+    [0,5,0,0,0,3,0,0,0],
+    [0,0,4,1,0,0,6,8,5],
+    [0,0,0,0,0,0,0,0,9],
+    [2,0,0,0,9,0,0,0,4]
+]
+
+def print_sudoku
+
+# ----- Fin -----
